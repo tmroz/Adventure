ed746805104b98016799398f5373a7b550d7a5b4
tmroz <tom.mroz@d214.org>
2014-05-14 09:11:56 -0500

    First Time

---
+++ Adventure/EndingViewController.h
@@ -0,0 +1,13 @@
+//
+//  EndingViewController.h
+//  Adventure
+//
+//  Created by Thomas M. Mroz on 5/14/14.
+//  Copyright (c) 2014 BGHS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EndingViewController : UIViewController
+
+@end
---
+++ Adventure/StoryViewController.h
@@ -0,0 +1,13 @@
+//
+//  StoryViewController.h
+//  Adventure
+//
+//  Created by Thomas M. Mroz on 5/13/14.
+//  Copyright (c) 2014 BGHS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StoryViewController : UIViewController
+
+@end
