5d10cbd92a58b30df0837b37fe991d409fc4bf70
tmroz <tom.mroz@d214.org>
2014-05-13 20:37:18 -0500

    Initial Commit

---
+++ Adventure/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Adventure
+//
+//  Created by Thomas M. Mroz on 5/13/14.
+//  Copyright (c) 2014 BGHS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
